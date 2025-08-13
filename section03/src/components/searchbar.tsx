bb19c4f2bfce2ffaaff549fc6ee21329dddf1db5
dohee12 <happyddo42@gmail.com>
2025-08-13 15:25:14 +0900

    feat: app Router 한입북스 UI 구현

--- section03/src/components/searchbar.tsx
+++ section03/src/components/searchbar.tsx
@@ -1,22 +1,38 @@
 "use client";
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import style from "./searchbar.module.css";
 
 export default function Searchbar() {
-  const Router = useRouter();
-  const [search, setShearch] = useState("");
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const [search, setSearch] = useState("");
+
+  const q = searchParams.get("q");
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setShearch(e.target.value);
+    setSearch(e.target.value);
   };
 
   const onSubmit = () => {
-    Router.push(`search?q=${search}`);
+    if (!search || q === search) return;
+    router.push(`/search?q=${search}`);
+  };
+
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
   };
 
   return (
-    <div>
-      <input value={search} onChange={onChangeSearch} />
+    <div className={style.container}>
+      <input value={search} onChange={onChangeSearch} onKeyDown={onKeyDown} />
       <button onClick={onSubmit}>검색</button>
     </div>
   );
