86491701be31a38ddbf3ec65e049d5abd6ae1cc2
dohee12 <happyddo42@gmail.com>
2025-08-19 11:50:24 +0900

    feat: 데이터 페칭 (앱 라우터 데이터 페칭, 데이터 캐시, 리쿼스트 메모이제이션)

---
+++ section04/src/app/(with-searchbar)/page.tsx
@@ -0,0 +1,55 @@
+import BookItem from "@/components/book-item";
+import style from "./page.module.css";
+import { BookData } from "@/types";
+
+async function AllBooks() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, {
+    cache: "no-cache",
+  });
+  if (!res.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const allbooks: BookData[] = await res.json();
+
+  return (
+    <div>
+      {allbooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+async function RecoBook() {
+  const req = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`,
+    { next: { revalidate: 3 } }
+  );
+  if (!req.ok) {
+    return <div>오류가 발생했습니다...</div>;
+  }
+  const randombooks: BookData[] = await req.json();
+
+  return (
+    <div>
+      {randombooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
+}
+
+export default async function Home() {
+  return (
+    <div className={style.container}>
+      <section>
+        <h3>지금 추천하는 도서</h3>
+        <RecoBook />
+      </section>
+      <section>
+        <h3>등록된 모든 도서</h3>
+        <AllBooks />
+      </section>
+    </div>
+  );
+}
